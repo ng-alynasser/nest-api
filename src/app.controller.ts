cbb1f17cfa59c2385cfd482a9b09d9510c819234
Aly Nasser <ng.alynasser@gmail.com>
2019-10-04 04:10:24 +0200

    blah

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Header } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,10 +6,9 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @Header('Content-Type', 'text/html')
   getHello(): {name: string} {
     return {
-      name: 'Aly Nasser'
+      name: 'NestJS API'
     };
   }
 }
